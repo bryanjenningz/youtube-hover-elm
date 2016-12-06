16905a14c11c814fb02f29e37685fc964e507f26
Bryan Jennings <bryanjenningz@gmail.com>
2016-12-06 15:34:49 -0800

    move English textTimes to JavaScript

--- src/Main.elm
+++ src/Main.elm
@@ -33,19 +33,21 @@ type alias Model =
   { hoverIndex : Int
   , youtubeTime : Float
   , timeTranslations : List TimeTranslation
+  , textTimes : List TextTime
   }
 
 type alias Flags =
   { timeTranslations : List TimeTranslation
+  , textTimes : List TextTime
   }
 
 init : Flags -> ( Model, Cmd Msg )
 init flags =
-  ( Model -1 0 flags.timeTranslations, Cmd.none )
+  ( Model -1 0 flags.timeTranslations flags.textTimes, Cmd.none )
 
 view model =
   div [class "text-center"]
-    [ h4 [] [text (getCurrentTextTime englishSubs model.youtubeTime).text]
+    [ h4 [] [text (getCurrentTextTime model.textTimes model.youtubeTime).text]
     , viewTranslatedSubs model
     ]
 
@@ -115,78 +117,3 @@ port youtubeTime : (Float -> msg) -> Sub msg
 
 subscriptions model =
   youtubeTime UpdateYoutubeTime
-
-englishSubs : List TextTime
-englishSubs =
-  [ { time = 0, text = "Today..." }
-  , { time = 4.7, text = "I will be reborn" }
-  , { time = 6.2, text = "There's a reason why I woke up this early to go to Tokyo." }
-  , { time = 12.1, text = "My bangs today look terrible too ..." }
-  , { time = 14.1, text = "Wearing traditional Japanese footwear..." }
-  , { time = 15.8, text = "I bought gifts at the store while I was drowsy." }
-  , { time = 18.6, text = "Then, I headed towards the bullet-train platform" }
-  , { time = 20.6, text = "Got on the train," }
-  , { time = 21.7, text = "Fell asleep at the moment I got to my seat." }
-  , { time = 24, text = "There's only one place I'm heading to." }
-  , { time = 25.7, text = "The legendary beauty salon," }
-  , { time = 27, text = "\"Ocean Tokyo\"" }
-  , { time = 28.4, text = "I've asked the dream-team here to assist me in dealing with my extraordinarily damaged hair" }
-  , { time = 34.3, text = "Regenerate! My spirit!" }
-  , { time = 36.6, text = "Wow. Your bangs are quite something!" }
-  , { time = 37.8, text = "I'm begging you to fix this!" }
-  , { time = 39.7, text = "Would you like some counseling?" }
-  , { time = 42.9, text = "Mr. Moru will be shampooing your hair now" }
-  , { time = 45.5, text = "Hold on, Mr. Moru's going to do it? I'm so nervous!" }
-  , { time = 48.4, text = "you've been filming all this time?" }
-  , { time = 50.4, text = "Yes" }
-  , { time = 51.6, text = "*Since nobody is watching me, I'm just going to chill here*" }
-  , { time = 67.2, text = "Please sit up" }
-  , { time = 68.1, text = "Thanks!" }
-  , { time = 69.4, text = "Watch your step!" }
-  , { time = 70.8, text = "Do you have any particular requests as to how you want your hair to look?" }
-  , { time = 73.1, text = "First, I wanna fix my bangs" }
-  , { time = 76.1, text = "How about color? Do you want it light? Dark?" }
-  , { time = 78.5, text = "I'm in a dilemma...Is lighter better?" }
-  , { time = 84.9, text = "Let's see how much I can transform myself!" }
-  , { time = 86.9, text = "Has it been a while since you last cut your hair?" }
-  , { time = 91.4, text = "Other than the front part, it's been a while..." }
-  , { time = 93.4, text = "Your bangs..." }
-  , { time = 94.2, text = "I cut it while filming one of my videos..." }
-  , { time = 97.9, text = "It's quite long compared to other people right?" }
-  , { time = 102.1, text = "*He can't do much besides laugh at my hair*" }
-  , { time = 104.1, text = "Please make me the most handsome man in the world" }
-  , { time = 108.7, text = "Your bangs are one heck of a thing (I'll take that as a compliment)" }
-  , { time = 111.3, text = "*I was complimented!!!*" }
-  , { time = 115.2, text = "It's disorganized and the widths are different too...." }
-  , { time = 117.2, text = "It's quite a disaster." }
-  , { time = 120.3, text = "Do as you please." }
-  , { time = 129.4, text = "(His Yu-Gi-Oh cards...)" }
-  , { time = 134.2, text = "3 years later" }
-  , { time = 139.5, text = "I look like a delinquent!" }
-  , { time = 143.1, text = "(To put color in my hair, we first bleached it)" }
-  , { time = 144.6, text = "Last year, I had blonde/bleached hair for a while" }
-  , { time = 146.6, text = "Brings back memories. It was actually similar to this color" }
-  , { time = 149.2, text = "(Sorry, I felt so comfortable that I fell asleep)" }
-  , { time = 157.1, text = "He's fast asleep" }
-  , { time = 158.5, text = "3 more years later" }
-  , { time = 160.4, text = "Rinsing my hair out" }
-  , { time = 164, text = "And this is the result..." }
-  , { time = 167.9, text = "Wow!!Thanks!" }
-  , { time = 171.1, text = "What an amazing transformation!" }
-  , { time = 174.6, text = "What color is this? It's looks like a pinkish color" }
-  , { time = 177.7, text = "You seem nervous!" }
-  , { time = 179.3, text = "Of course I'm nervous!" }
-  , { time = 181.3, text = "(My best pose)" }
-  , { time = 184.3, text = "We actually cut a big chunk of your hair" }
-  , { time = 186.4, text = "Thank you very much!" }
-  , { time = 194.1, text = "So, without incident, I was able to fix my terrible hairstyle" }
-  , { time = 199, text = "Thank you for making me look so much better!" }
-  , { time = 201.4, text = "How were my bangs?" }
-  , { time = 202.6, text = "It was hard, so I'd like you to grow it out a little" }
-  , { time = 205.5, text = "I will" }
-  , { time = 207.6, text = "To all the viewers, come visit us at OceanTokyo Harajuku!" }
-  , { time = 210.5, text = "We'll be waiting!" }
-  , { time = 220.1, text = "This is so cautious" }
-  , { time = 221.2, text = "I'm not being cautious!" }
-  , { time = 222.7, text = "Feeling shy suddenly..." }
-  ]
